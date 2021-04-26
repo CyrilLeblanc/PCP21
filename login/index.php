684099c7b8e594536a02a5eaad5346245c4c308f
CyrilLeblanc <cyril_leblanc@outlook.fr>
2021-04-26 16:27:07 +0200

    nouveau fichier : bootstrap.html 	modifié :         footer.html 	modifié :         login/index.php 	supprimé :        login/style.css

--- login/index.php
+++ login/index.php
@@ -18,7 +18,7 @@ if ($_SESSION['idCovoitureur'] != 0 && $_SESSION['is_Confirme'] == 1)
 // on vérifie les informations de connection du formulaire
 if (isset($_POST['email']) && isset($_POST['password']))
 {
-	require_once '../config.php';
+	require_once './config.php';
 
 	// on récupère les infos de compte à partir du mail
 	$email = $_POST['email'];
@@ -56,54 +56,95 @@ if (isset($_POST['email']) && isset($_POST['password']))
 
 ?>
 
-
-
+<doctype HTML>
+<html>
+<head>
 <!--
-#
 # Codée par : Cyril LEBLANC
-# Date : 
+# Date : 26/03/2021
 # Description : Page de connexion à la plateforme de covoiturage participatif
-# Note : Pas d'utilisation de Bootstrap
 # Modification : NONE
 #
 -->
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="./style.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Se connecter - PCP21</title>
-	</head>
-	<body>
-		<header>
-			<h1>Covoiturage Participatif Charles Poncet</h1>
-		</header>
-		<main>
-			<div class="container">
-				<form method="post" action="">
-					<div class="element">
-						<label for="email">E-mail :</label><br/>
-						<input type="email" name="email" id="email" required></input>
-					</div>
-					<div class="element">
-						<label for="password">Mot de passe :</label><br/>
-						<input type="password" name="password" id="password" required></input>
-					</div>
-				
-					<div class="element">
-						<input type="checkbox" name="remember" id="remember">
-						<label for="remember">Se souvenir de moi</label>
-					</div>
-					<div class="element">
-						<button type="submit">Se connecter</button>
-					</div>
-				</form>
-				Si vous n'avez pas de compte vous pouvez <a href="#">vous inscrire</a>.
+	<?php include_once '../bootstrap.html'; ?>
+	<style>
+	.center{
+		padding: auto;
+		margin-top: 0%;
+	}
+
+	a
+	{
+		font-weight: bold;
+	}
+
+	.form-control
+	{
+		max-width:75%;
+	}
+
+	</style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-success justify-content-center text-white">
+<ul class="navbar-nav">
+	<li class="nav-item">
+	  <h1>Covoiturage Participatif Charles Poncet</h1>
+	</li>
+</ul>
+</nav>
+<?php 
+	// affichage des erreurs de connection
+	if ($_GET['error'] == 'login')
+	{
+		echo "<BR/>
+		<div class='alert alert-danger text-center'>
+		<strong>Erreur :</strong> Mauvais mot de passe ou mauvaise adresse e-mail.
+	  	</div>";
+	} elseif($_GET['error'] == 'confirme')
+	{
+		echo "<BR/>
+		<div class='alert alert-danger text-center'>
+		<strong>Erreur :</strong> Compte non confirmé merci de contacté le webmaster en cas de problème.
+	  	</div>";
+	}
+
+?>
+
+<br/>
+<div class="container border shadow rounded center bg-success" style="max-width: 40em;">
+<br/>
+	<div class="container border shadow rounded text-center bg-white">
+
+	<form method="POST">
+		<div class="d-flex flex-wrap">
+			<div class="form-group p-2 flex-fill">
+			<label for="email">Adresse e-mail</label></br>
+			<input name="email" type="email" required id="email" class="form-control mx-auto"></input></br>
 			</div>
 
+			<div class="form-group p-2 flex-fill">
+			<label for="password">Mot de passe</label></br>
+			<input name="password" type="password" required id="password" class="form-control mx-auto"></input></br>
+			</div></br>
+		</div>
 
-			
-		</main>
-		<?php include_once '../footer.html'; ?>
-	</body>
+		<div class="custom-control custom-checkbox">
+		<input type="checkbox" id="remember" name="remember" value="1" class="custom-control-input"></input>
+		<label for="remember" class="custom-control-label"> Se souvenir de moi.</label><br/><br/>
+		</div>
+
+		<button class="btn btn-success" type="submit">Se connecter</button>
+		
+	</form>
+	<p><a href='#TODO'><p>J'ai oublié mon mot de passe.</a></br>
+	Si vous n'avez pas de compte vous pouvez <a href="TODO">vous inscrire</a>.</p>
+
+	</div>
+<br/>
+</div>
+<br/><br/>
+<?php include_once '../footer.html'; ?>
+</body>
 </html>
