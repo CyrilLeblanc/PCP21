2fbfe3c303f29c1720c7e9d13bcd414067cce0e5
Quentin Villanova <quentinv74@iMac-de-Quentin.local>
2021-05-23 00:22:57 +0200

    modified:   webmaster/creer_nouveau_point/index.php 	deleted:    webmaster/creer_nouveau_point/upload.php

--- webmaster/creer_nouveau_point/index.php
+++ webmaster/creer_nouveau_point/index.php
@@ -8,12 +8,11 @@
 
     if(isset($_POST['nom']) && isset($_POST['adresse']) && isset($_POST['ville']) && isset($_POST['latitude']) && isset($_POST['longitude']))
     {
-
-      if(isset($_POST['submit']))
+      if(isset($_POST['submit']) && !$_FILES['photo'] == null)
       {
         $filename = $_FILES['photo']['name'];
         $tempname = $_FILES['photo']['tmp_name'];
-        $filesize = $_FILES['photo']['size';]
+        $filesize = $_FILES['photo']['size'];
         $error = 1;
         $folder = "../images/".$filename;
 
@@ -27,15 +26,11 @@
 
         if(move_uploaded_file($tempname,$folder)) 
         {
-          $_POST['photo'] = $newname;
-        }
-        elseif($filename == null)
-        {
-          $_POST['photo'] = "";
+          $_FILES['photo'] = $newname;
         }
         else 
         { 
-          $_POST['photo'] = "";
+          $_FILES['photo'] = "";
 
           echo "</br></br>
             <div class='alert alert-danger text-center'>
@@ -43,14 +38,44 @@
             </div>"; 
         }
 
+        if($filesize > 5000000)
+        {
+          echo "</br></br>
+            <div class='alert alert-danger text-center'>
+            <h2><strong>Le fichier dépasse la taille maximale de 5Mo.</strong></h2>
+            </div>";
+          $error = 0;
+        }
+
+        if($ext != "jpg" && $ext != "jpeg" && $ext != "png")
+        {
+          echo "</br></br>
+            <div class='alert alert-danger text-center'>
+            <h2><strong>Seulement les fichiers .jpg, .jpeg et .png sont acceptés.</strong></h2>
+            </div>";
+          $error = 0;
+        }
+
+        if ($error == 0) 
+        {
+          if(!isset($_FILES['photo']))
+          {
+            unlink($folder);
+          } 
+          $newname = null;
+          
+          echo "</br></br>
+            <div class='alert alert-danger text-center'>
+            <h2><strong>Votre image n'a pas été ajoutée.</strong></h2>
+            </div>";
+        }
       }
-      
+
       //Ajout du Point de RDV
       $Ajouter_Point_RDV = new Point();
-      $Ajouter_Point_RDV->add_Point($_POST['nom'],$_POST['adresse'],$_POST['ville'],$_POST['latitude'],$_POST['longitude'],$_POST['photo'],1);
+      $Ajouter_Point_RDV->add_Point($_POST['nom'],$_POST['adresse'],$_POST['ville'],$_POST['latitude'],$_POST['longitude'],$newname,1);
 
-      if($filename == null){}
-      else 
+      if(!$_FILES['photo'] == null && $error == 1)
       {
         rename($folder, $newfolder);
       }
@@ -130,70 +155,6 @@
                 </div>";
             }
           }
-
-
-          /*//Gestion de l'image
-          $target_dir = "../images/";
-          $target_file = $target_dir . basename($_FILES["photo"]["name"]);
-          $uploadOk = 1;
-          $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-          $fullPath = "/PCP21/webmaster/images/" . $_POST['photo'];
-
-          echo $fullPath;
-
-          //Vérifie s'il s'agit vraiment d'une image
-          if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["photo"]["tmp_name"]);
-            if($check !== false) {
-              $uploadOk = 1;
-            } else {
-              echo "</br></br>
-                <div class='alert alert-danger text-center'>
-                <h2><strong>Le fichier n'est pas une image.</strong></h2>
-                </div>";
-              $uploadOk = 0;
-            }
-          }
-          //Vérifie la taille de l'image
-          if ($_FILES["photo"]["size"] > 2000000) { //ToDO
-            echo "</br></br>
-              <div class='alert alert-danger text-center'>
-              <h2><strong>Le fichier dépasse la taille maximale.</strong></h2>
-              </div>";
-            $uploadOk = 0;
-          }
-
-          //Autorise certains formats de fichiers
-          if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            echo "</br></br>
-              <div class='alert alert-danger text-center'>
-              <h2><strong>Seulement les fichiers .jpg, .jpeg et .png sont acceptés.</strong></h2>
-              </div>";
-            $uploadOk = 0;
-          }
-
-          $new_name = $_POST["nom"] . '_' . $_POST["ville"] . '.' . $imageFileType;
-
-          //Vérifie s'il y a eu une erreur
-          if ($uploadOk == 0) {
-            echo "</br></br>
-              <div class='alert alert-danger text-center'>
-              <h2><strong>Il y a eu une erreur avec votre fichier.</strong></h2>
-              </div>";
-          //Si tout est ok, on upload l'image
-          } else {
-            if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_dir . $new_name)) {
-              echo "</br></br>
-              <div class='alert alert-success text-center'>
-              <h5><strong>Le fichier ". htmlspecialchars(basename($new_name)). " a bien été uploadé.</strong></h5>
-              </div>";
-            } else {
-              echo "</br></br>
-                <div class='alert alert-danger text-center'>
-                <h2><strong>Il y a une erreur.</strong></h2>
-                </div>";
-            }
-          }*/
         ?>
 
       </div>
