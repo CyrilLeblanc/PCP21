81ed3fb1b32e48f63a869d57923131ed1a923e7a
Quentin Villanova <quentinv74@iMac-de-Quentin.local>
2021-05-23 10:58:39 +0200

    modified:   webmaster/creer_nouveau_point/index.php 	new file:   webmaster/images/avec photo_avec photo.jpeg 	new file:   webmaster/images/iPad avec photo_iPad avec photo.jpeg 	new file:   webmaster/images/iPhone avec photo_iPhone avec photo.jpeg 	modified:   webmaster/modifier_point/index.php

--- webmaster/creer_nouveau_point/index.php
+++ webmaster/creer_nouveau_point/index.php
@@ -91,7 +91,6 @@
 <body>
   <div class="container p-3 my-3 border shadow rounded" align="center">
 
-
     <div class="container bg-success p-2 my-2 rounded">
       <a href="/index.php">
         <button class="btn material-icons" style="color: white; font-size: 250%;">&#xe88a;</button>
--- webmaster/modifier_point/index.php
+++ webmaster/modifier_point/index.php
@@ -11,7 +11,6 @@
 </head>
 
 <body>
-
 	<div class="container p-3 my-3 border shadow rounded" align="center">
 
 	<div class="container bg-success p-2 my-2 rounded" >
@@ -22,7 +21,7 @@
 	</div>
 
 		<!-- TABLE -->
-		<div class="container overflow-auto" style="font-size: 10px; height: 400px;">
+		<div class="container overflow-auto" style="font-size: 10px; height: 450px;">
 		<table class="table">
 
 
@@ -55,7 +54,7 @@
 
 								<td> <a href="https://www.google.com/maps/place/' . $value["Latitude"] . ',' . $value["Longitude"] . '" onclick="window.open(this.href); return false;"" style="font-weight: bold; color: green;">' . $value["Latitude"] . '<br/>' . $value["Longitude"] . '</td>
 
-								<td> <a href="' . $value["Point_Image"] . '"onclick="window.open(this.href); return false;"> <img src="' . $value["Point_Image"] . '"class="img-fluid rounded" width="150"></img></a> </td>
+								<td> <a href="' . $value["Point_Image"] . '"onclick="window.open(this.href); return false;"> <img src="' . $value["Point_Image"] . '"class="img-fluid rounded" width="100"></img></a> </td>
 
 								<td>' . '<button class="btn btn-success material-icons btn-sm" style="font-size: 150%" onclick="popupModifPoint(`' . $value["Nom"] . '`,`' . $value["Adresse"] . '`,`' . $value["Ville"] . '`,`' . $value["Latitude"] . '`,`' . $value["Longitude"] . '`,`' . $value["Point_Image"] . '`)" data-toggle="modal" data-target="#popupModif">&#xe3c9;</button>' . '</td>
 							</tr>';
