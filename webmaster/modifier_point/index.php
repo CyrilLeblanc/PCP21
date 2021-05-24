51d2b851f9a9111d6bf7d651bb714529f8841c3f
Quentin Villanova <quentinv74@iMac-de-Quentin.local>
2021-05-24 18:00:31 +0200

    modified:   webmaster/modifier_point/index.php

--- webmaster/modifier_point/index.php
+++ webmaster/modifier_point/index.php
@@ -94,33 +94,33 @@
 
 						<div class="form-group" align="left">
 							<label for="adressePoint" class="mr-sm-2">Adresse : </label><br/>
-							<input type="text" class="mb-2 mr-sm-2" placeholder="1 avenue de Charles Poncet" id="adressePoint" name="adressePoint" value="" required>
+							<input type="text" class="mb-2 mr-sm-2" id="adressePoint" name="adressePoint" value="" required>
 
 						</div>
 
 						
 						<div class="form-group" align="left">
 							<label for="villePoint" class="mr-sm-2">Ville : </label><br/>
-							<input type="text" class="mb-2 mr-sm-2" placeholder="74300 Cluses" id="villePoint" name="villePoint" value="" required>
+							<input type="text" class="mb-2 mr-sm-2" id="villePoint" name="villePoint" value="" required>
 
 						</div>
 
 						<div class="form-group" align="left">
 							<label for="latitudePoint" class="mr-sm-2">Latitude : </label><br/>
-							<input type="text" class="mb-2 mr-sm-2" placeholder="46.0618" id="latitudePoint" name="latitudePoint" value="" required>
+							<input type="text" class="mb-2 mr-sm-2" id="latitudePoint" name="latitudePoint" value="" required>
 
 						</div>
 
 						<div class="form-group" align="left">
 							<label for="longitudePoint" class="mr-sm-2">Longitude : </label><br/>
-							<input type="text" class="mb-2 mr-sm-2" placeholder="6.57994" id="longitudePoint" name="longitudePoint" value="" required>
+							<input type="text" class="mb-2 mr-sm-2" id="longitudePoint" name="longitudePoint" value="" required>
 
 						</div>
 
 						<div class="form-group" align="left">
 							<label for="imagePoint" class="mr-sm-2">Photo : </label><br/>
 							<img src="" id="lienImage" class="img-fluid rounded" width="200"><br/>
-							<input type="file" class="mb-2 mr-sm-2" placeholder="../images/" id="imagePoint" name="imagePoint" value="">
+							<input type="file" class="mb-2 mr-sm-2" id="imagePoint" name="imagePoint" value="">
 
 						</div>
 					</div>
