ac215ddc85bd3ed2248b535a5226c0291ee1be8f
Quentin Villanova <quentin@MacBook-Air-de-Quentin.local>
2021-06-02 14:14:52 +0200

    modified:   config.php 	modified:   request/Point.php 	modified:   webmaster/.DS_Store 	modified:   webmaster/creer_nouveau_covoiturage/index.php 	modified:   webmaster/modifier_point/index.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
 //      ****************************************************
 
 $adress = '127.0.0.1';      // addresse du serveur. En cas d'erreur mettre 'localhost'
-$user = 'admin';            // nom d'utilisateur
-$password = 'test';         // mot de passe utilisateur
+$user = 'root';            // nom d'utilisateur
+$password = '';         // mot de passe utilisateur
 $bdd = 'PCP21';       // nom de la base de donnée
 
 
--- webmaster/modifier_point/index.php
+++ webmaster/modifier_point/index.php
@@ -15,12 +15,12 @@
 <body>
 	<div class="container p-3 my-3 border shadow rounded" align="center">
 
-	<div class="container bg-success p-2 my-2 rounded" >
-		<a href="../../accueil">
-		<button class="btn material-icons" style="color: white; font-size: 250%;">&#xe88a;</button>
-		</a>
-		<h2 class="text-center" style="color: white;">Modifier Point RDV</h2>
-	</div>
+		<div class="container bg-success p-2 my-2 rounded" >
+			<a href="../../accueil">
+			<button class="btn material-icons" style="color: white; font-size: 250%;">&#xe88a;</button>
+			</a>
+			<h2 class="text-center" style="color: white;">Modifier Point RDV</h2>
+		</div>
 
 		<!-- TABLE -->
 		<div class="container overflow-auto" style="font-size: 12px; height: 450px;">
