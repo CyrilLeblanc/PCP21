7ad5de00eda5b2812b87b16cb3ff03d038c0d001
Quentin Villanova <quentinv74@iMac-de-Quentin.local>
2021-06-06 11:18:56 +0200

    modified:   accueil/index.php 	modified:   bootstrap.php 	modified:   header.php 	deleted:    images/test_test.jpeg 	modified:   verif_session.php 	renamed:    webmaster/creer_etape/index.php -> webmaster/creer_nouvelle_etape/index.php

--- accueil/index.php
+++ accueil/index.php
@@ -2,7 +2,7 @@
   session_start();
   require_once '../verif_session.php';
   ini_set('display_errors', 1);   #DEBUG
-ini_set('display_startup_errors', 1);   #DEBUG
+  ini_set('display_startup_errors', 1);   #DEBUG
 ?>
 
 
